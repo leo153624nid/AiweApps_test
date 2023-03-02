f40091495c3f195b5a23b095bace2282d2ceca5b
Алексей Чайкин <barjastroy@gmail.com>
2023-03-02 10:20:24 +0300

    Update PrimaryButton.swift

--- AiweApps_test/PrimaryButton.swift
+++ AiweApps_test/PrimaryButton.swift
@@ -8,27 +8,36 @@
 import UIKit
 
 class PrimaryButton: UIButton {
-    var buttonIsOn = false { // todo
+    var buttonIsOn = false {
         didSet {
             self.backgroundColor = buttonIsOn ? UIColor.black : UIColor.lightGray
-            self.setTitleColor(buttonIsOn ? UIColor.lightGray : UIColor.black, for: .normal)
+            self.setTitleColor(buttonIsOn ? UIColor.white : UIColor.black, for: .normal)
         }
     }
     
-//    button.addTarget(<#T##target: Any?##Any?#>, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+    private var buttonName: String
     
     init(title: String) {
+        self.buttonName = title
         super.init(frame: .zero)
         self.translatesAutoresizingMaskIntoConstraints = false
         self.backgroundColor = .lightGray
         self.layer.cornerRadius = 12
         self.setTitleColor(.black, for: .normal)
         self.setTitleColor(.yellow, for: .highlighted)
+        self.setTitle("Обработка", for: .highlighted)
         self.setTitle(title, for: .normal)
+        self.addTarget(self, action: #selector(buttonIsTapped), for: .touchUpInside)
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func buttonIsTapped(sender: UIButton) {
+        print("\(buttonName)-button is tapped")
+        buttonIsOn.toggle()
+        NotificationCenter.default.post(Notification.init(name: buttonName == ButtonNames.parse.rawValue ? .notificationFromParseButton : .notificationFromShowButton))
     }
 }
 
